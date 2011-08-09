e7fcb817f8bed2bfcf5a4d4135b5bb7e8ec49b07
Doug Seifert <doug@seifert.net>
2011-08-08 23:08:01 -0700

    Add game history to player show

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -2,4 +2,17 @@ class Entrant < ActiveRecord::Base
   belongs_to :game
   belongs_to :player
   acts_as_list :scope => :game
+
+  def score
+    case final_position
+    when 1
+      3
+    when 2
+      2
+    when 3
+      1
+    when game.entrants.size
+      -1
+    end
+  end
 end
